d70929ffd4b108eae5d2777c2a87e40ce1aa8fa2
Nawaritta <nor.benhaddou@gmail.com>
2024-02-08 19:26:02 +0000

    5 fields

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,7 +7,7 @@ from typing import List
 import mysql.connector
 
 
-PII_FIELDS = ('ssn', 'email', 'phone', 'password')
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
