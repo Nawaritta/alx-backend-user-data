994820b437038fc6a811fafa1299670fe98e30bd
Nawaritta <nor.benhaddou@gmail.com>
2024-02-20 12:11:18 +0000

    return None if not found

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -40,6 +40,16 @@ class Auth:
         return bcrypt.checkpw(password.encode('utf-8'),
                               user.hashed_password)
 
-    def _generate_uuid() -> str:
+    def _generate_uuid(self) -> str:
         """ Returns a string representation of a new UUID."""
         return str(uuid.uuid4())
+
+    def create_session(self, email: str) -> str:
+        """returns the session ID as a string"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return None
+        session_id = self._generate_uuid()
+        self._db.update_user(user.id, session_id=session_id)
+        return session_id
