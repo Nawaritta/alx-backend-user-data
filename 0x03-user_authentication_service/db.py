c78ddd1db1cb0c546d7d9ac32eefabfa9392c105
Nawaritta <nor.benhaddou@gmail.com>
2024-02-21 14:01:49 +0000

    use sqlalchemy.orm.exc

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,8 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.orm.exc import NoResultFound
-from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound, InvalidRequestError
 from user import Base, User
 from typing import Dict
 
@@ -44,7 +43,7 @@ class DB:
 
         return new_user
 
-    def find_user_by(self, **kwa) -> User:
+    def find_user_by(self, **kwa: Dict) -> User:
         """returns the first row found in the users table as filtered
         by the method's input arguments"""
 
